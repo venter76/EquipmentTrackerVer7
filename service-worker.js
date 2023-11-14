98bc51db21231e863516038ae5d087cb0fb9fd3a
Stephen <venter76@gmail.com>
2023-11-14 19:08:10 +0200

    1

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'static-cache-v16';
+const CACHE_NAME = 'static-cache-v17';
 const STATIC_ASSETS = [
     'placeholder.html',
     '/iconLarge_1.png',
     '/iconLarge_2.png',
     '/iconLarge_3.png',
-    
+    '/token.ejs',
     '/MrF.gif',
     '/NXRX.gif',
     '/penguin.gif',
